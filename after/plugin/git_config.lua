905ca6fdffc03edb42ef1ec3e543c469d1a02a82
Alvaro Escobar <alvaro.escobar@unmsm.edu.pe>
2023-07-16 13:46:06 -0500

    fix(gitsigns): update keymaps

--- after/plugin/git_config.lua
+++ after/plugin/git_config.lua
@@ -47,14 +47,70 @@ require("gitsigns").setup({
 	yadm = {
 		enable = false,
 	},
-	keymaps = {
+	--[[ keymaps = {
 		noremap = true,
 		["n <leader>gp"] = '<cmd> lua require"gitsigns".preview_hunk()<CR>',
 		["n <leader>gi"] = '<cmd> lua require"gitsigns".blame_line()<CR>',
 		["n pc"] = { expr = true, "&diff ? '[c' : '<cmd> lua require\"gitsigns\".prev_hunk()<CR>'" },
 		["n nc"] = { expr = true, "&diff ? ']c' : '<cmd> lua require\"gitsigns\".next_hunk()<CR>'" },
 		-- ['n ghr'] = '<cmd> lua require"gitsigns".toggle_deleted()<CR>',
-	},
+	}, ]]
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "nc", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "pc", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		-- Actions
+		-- map("n", "<leader>hs", gs.stage_hunk)
+		-- map("n", "<leader>hr", gs.reset_hunk)
+		--[[ map("v", "<leader>hs", function()
+			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end) ]]
+		--[[ map("v", "<leader>hr", function()
+			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end) ]]
+		-- map("n", "<leader>hS", gs.stage_buffer)
+		-- map("n", "<leader>hu", gs.undo_stage_hunk)
+		-- map("n", "<leader>hR", gs.reset_buffer)
+		map("n", "<leader>gp", gs.preview_hunk)
+		map("n", "<leader>gi", function()
+			gs.blame_line({ full = true })
+		end)
+		-- map("n", "<leader>tb", gs.toggle_current_line_blame)
+		-- map("n", "<leader>hd", gs.diffthis)
+		--[[ map("n", "<leader>hD", function()
+			gs.diffthis("~")
+		end) ]]
+		-- map("n", "<leader>td", gs.toggle_deleted)
+
+		-- Text object
+		-- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+	end,
 })
 
 vim.cmd([[nnoremap <expr> nc &diff ? ']c' : '[c']])
