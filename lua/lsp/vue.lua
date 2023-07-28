71412a9c1b18880d08518c325638c1137f8bd5d5
Alvaro Escobar <alvaro.escobar@unmsm.edu.pe>
2023-07-28 10:58:24 -0500

    feat(lsp): add new vue configuration and defaults keymaps

--- lua/lsp/vue.lua
+++ lua/lsp/vue.lua
@@ -113,6 +113,25 @@ local volar_root_dir = "/home/jose/.local/share/nvim/lsp_servers/volar" ]]
 } ]]
 -- lspconfig.volar_html.setup({})
 
+local util = require("lspconfig.util")
+local function get_typescript_server_path(root_dir)
+	local global_ts = vim.fn.stdpath("data") .. "/mason/packages/typescript-language-server/node_modules/typescript/lib"
+	-- Alternative location if installed as root:
+	-- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+	local found_ts = ""
+	local function check_dir(path)
+		found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+		if util.path.exists(found_ts) then
+			return path
+		end
+	end
+	if util.search_ancestors(root_dir, check_dir) then
+		return found_ts
+	else
+		return global_ts
+	end
+end
+
 return {
 	setup = function(keymaps, capabilities, enabled_capabilities)
 		-- capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
@@ -129,12 +148,12 @@ return {
 			on_new_config = function(new_config, new_root_dir)
 				new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
 			end,
-			init_options = {
+			--[[ init_options = {
 				typescript = {
 					tsdk = vim.fn.stdpath("data")
 						.. "/mason/packages/typescript-language-server/node_modules/typescript/lib",
 				},
-			},
+			}, ]]
 		})
 	end,
 }
