2adb6a67e1c578d6c0fd3be5e6bb220031ba62a5
Alvaro Escobar <alvaro.escobar@unmsm.edu.pe>
2023-10-09 09:13:48 -0500

    fix(lint): fixed lint configuration for eslint_d

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,16 +3,19 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
-		lint.formatters_by_ft = {
+		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 			vue = { "eslint_d" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
-			pattern = { "*.ts", "*.js", "*.vue" },
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
 				lint.try_lint()
